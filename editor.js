9a8294fc569469a33acbb89a9741977a4134ccd3
Cairo Caplan <cairo.caplan@eclipse-foundation.org>
2026-08-19 16:18:13 +0200

    [ips][ip_card] IP Card dir creation, local IP Card example for ISOLDE and uniformization of project file names to lowercase

--- editor.js
+++ editor.js
@@ -215,7 +215,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = `${projectName}.json`;
+    a.download = `${projectName.toLowerCase()}.json`;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
